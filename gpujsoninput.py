ccaa901d339d897738a137f3af4ad1b153255686
LimesKey <85136735+LimesKey@users.noreply.github.com>
2022-12-06 04:53:58 -0500

    fixed programs not continuning to next part, fixed overwriting te file, working on making new content go into the current "GPU" catagory

--- gpujsoninput.py
+++ gpujsoninput.py
@@ -26,29 +26,30 @@ while True:
 
         acceptable_formats = ['H', 'KH', 'MH', 'GH', 'TH']  # list of acceptable hashes
 
-        for x in acceptable_formats:  # for every element in array
-            if format_input == acceptable_formats or '/s'.join(
-                    acceptable_formats):  # if the format_input matches any one of the ones in acceptable_formats or
-                # acceptable_formats with a "/s" added onto it
-                break  # sucessful program
-            else:
-                print("Incorrect Input")  # try again
+        if format_input in acceptable_formats or format_input == '/s'.join(acceptable_formats):
+            # format_input is in acceptable_formats
+            break  # successful program
+        else:
+            # format_input is not in acceptable_formats
+            print("Incorrect Input")  # try again
 
     gflop = input("Enter GPFLOPs: ")
 
     # Create a dictionary with the data and append it to the list
-    gpu_dict = "GPUs", {
+    gpu_dict = {
         f"{gpu_model}": {
             "GFLOPS-64": gflop,
-            f"{ALGO} HASHRATE": hashrate + f"H/S"}
+            f"{ALGO} HASHRATE": hashrate + f"/S"}
     }
 
-    gpu_data.append(gpu_dict)
+gpu_data.append(gpu_dict)
 
 
 def write_json(data, filename='data.json'):
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
+    with open(filename, 'a+') as file:
+        if 'GPUs' not in file:
+            data.update({"GPUs", })
+    json.dump(data, file, indent=4)
 
 
 # Write the data to the JSON file
