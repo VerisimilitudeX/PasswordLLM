d218761fdd2c69e40fb7d5214745a0a3e0eba4ae
LimesKey <85136735+LimesKey@users.noreply.github.com>
2023-06-05 11:35:03 -0400

    Update test.rs

--- src/test.rs
+++ src/test.rs
@@ -5,6 +5,7 @@ mod test {
     fn test_pool_size_and_entropy() {
         use crate::calculate_entropy;
         use crate::GetPoolSize;
+        //use crate::main;
         // Test cases with different passcodes
         let test_cases = vec![
             ("password", 26, 38),   // Pool size: 26 (low_case) + 10 (digits), Entropy: 38
@@ -13,10 +14,9 @@ mod test {
         ];
 
         for (passcode, expected_pool_size, expected_entropy) in test_cases {
-            println!("Passcode: {}", passcode);
             let pool_size = GetPoolSize(passcode.to_string());
   
-            assert_eq!(pool_size[0], expected_pool_size, "Pool size is not correct {} != {}", pool_size[0], expected_pool_size);
+            assert_eq!(pool_size[0], expected_pool_size, "Pool size is not correct {} != {}, passcode = {:?}.", pool_size[0], expected_pool_size, passcode);
 
             let entropy = calculate_entropy(pool_size);
             assert_eq!(entropy, expected_entropy.try_into().unwrap())
