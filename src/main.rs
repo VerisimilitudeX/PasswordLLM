bee2d466026d30968b33800df0d0c530b95145e0
LimesKey <85136735+LimesKey@users.noreply.github.com>
2023-06-05 12:11:14 -0400

    Reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 mod test;
-use std::{env};
+use std::env;
 extern crate regex;
 use regex::Regex;
 
 // Entire file calculates the entropy
-pub fn main(){
+pub fn main() {
     let args: Vec<String> = env::args().collect();
     let password = &args[1];
 
-    let PoolSize = GetPoolSize(password.to_string());
-    let Entropy = calculate_entropy(PoolSize.clone());
-    println!("Entropy: {} bits", Entropy);
-    println!("Pool Size: {:?}", PoolSize[0].clone());
+    let pool_size = get_pool_size(password.to_string());
+    let entropy = calculate_entropy(pool_size.clone());
+    println!("Entropy: {} bits", entropy);
+    println!("Pool Size: {:?}", pool_size[0].clone());
 }
 
 // Pool size based on https://github.com/Kush-munot/Password-Strength-Checker
-pub fn GetPoolSize(password: String) -> Vec<u64> {
+pub fn get_pool_size(password: String) -> Vec<u64> {
     pub struct PoolTable {
         digits: bool,
         low_case: bool,
@@ -35,10 +35,11 @@ pub fn GetPoolSize(password: String) -> Vec<u64> {
 
         password_characteristics.low_case = Regex::new(r#"[a-z]"#).unwrap().is_match(&password);
         password_characteristics.up_case = Regex::new(r#"[A-Z]"#).unwrap().is_match(&password);
-        password_characteristics.digits = Regex::new(r#"[\d]"#).unwrap().is_match(&password); // Updates password_characteristics struct with bool values if password contains
+        password_characteristics.digits = Regex::new(r#"[\d]"#).unwrap().is_match(&password);
+        // Updates password_characteristics struct with bool values if password contains digits
         password_characteristics.special = Regex::new(r#"[^A-Za-z0-9\s]"#).unwrap().is_match(&password);
+        
         password_characteristics
-    
     }
 
     let pass_char = calculate(&password);
@@ -47,22 +48,24 @@ pub fn GetPoolSize(password: String) -> Vec<u64> {
     if pass_char.digits {
         pool_score += 10;
     }
-    
+
     if pass_char.low_case {
         pool_score += 26;
     }
-    
+
     if pass_char.up_case {
         pool_score += 26;
     }
-    
+
     if pass_char.special {
         pool_score += 32;
     }
-    
-    let score: Vec<u64> = vec![pool_score.try_into().unwrap(), password.chars().count().try_into().unwrap()];
-    score
 
+    let score: Vec<u64> = vec![
+        pool_score.try_into().unwrap(),
+        password.chars().count().try_into().unwrap(),
+    ];
+    score
 }
 
 pub fn calculate_entropy(pool_score: Vec<u64>) -> f64 {
