5c3c1a614024a0fe8775adc81cbe98ad0a0755f4
LimesKey <85136735+LimesKey@users.noreply.github.com>
2023-10-02 09:43:15 -0400

    Better print outs

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -3,7 +3,9 @@
 pub mod gpu { 
     use opencl3::device::{get_all_devices, CL_DEVICE_TYPE_GPU};
     use std::io;
-    pub fn obtainGPU() -> Result<(), ()> {
+    use crate::round;
+
+    pub fn obtainGPU<T>() -> Result<u32, T> {
         let devices: Vec<*mut std::ffi::c_void> = get_all_devices(CL_DEVICE_TYPE_GPU).expect("Cannot detect a GPU!");
         let mut gpu_choice: u32 = 0;
 
@@ -37,13 +39,16 @@ pub mod gpu {
         }
         println!("You have selected GPU Device {}", gpu_choice);
         let gpu = opencl3::device::Device::new(devices[gpu_choice as usize]);
+        let gpu_clock = gpu.max_clock_frequency().unwrap();
+        let gpu_cores = gpu.max_compute_units();
+        let gpu_cores = gpu_cores.unwrap(); // use ?
+        let gpu_cores = gpu_cores * 8;
 
+        let gpu_gflops: u32 = gpu_clock * gpu_cores * 2;
+
+        println!("Your GPU has a clock speed of {} MHz", gpu_clock);
         println!("Your {} has {} CUDA cores and {} stream multiprocessors", gpu.name().unwrap(), gpu.max_compute_units().unwrap(), (gpu.max_compute_units().unwrap() * 8));
-        //println!("Device: {:?}", device.name());
-        //println!("Device: {:?}", device.vendor());
-        //println!("Device: {:?}", device.version());
-        //println!("Device: {:?}", device.driver_version());
-        //println!("MAx compute units: {:?}", device.max_compute_units());
-        Ok(())
+        println!("This GPU has {} GFLOPS for FP32!", gpu_gflops.to_string()); // todo format properly
+        Ok(gpu_gflops)
     }
 }
