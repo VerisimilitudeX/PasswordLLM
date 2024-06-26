ab030ef449af72c1138b693ffa670973f7b052f2
LimesKey <talk@limeskey.com>
2024-06-26 15:26:52 -0400

    add old code that i lsot :Sob:

--- password-llm/src/utils.rs
+++
@@ -1,8 +0,0 @@
-pub mod pwned_api {
-    use crate::Error;
-
-    pub async fn pass_check(password: &str) -> Result<u64, Error> {
-        let pwned_api = pwned::api::PwnedBuilder::default().build().unwrap();
-        Ok(pwned_api.check_password(password).await?.count)
-    }
-}
